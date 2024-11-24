4bbfec0157a06707160c02229c8d0b677ce68e9d
Calypso <138980470+Zloits@users.noreply.github.com>
2024-11-24 12:30:18 +0700

    Update ObjectUpdaterTest.java

--- src/test/java/ObjectUpdaterTest.java
+++ src/test/java/ObjectUpdaterTest.java
@@ -13,27 +13,9 @@ public class ObjectUpdaterTest {
         OrionLib orionLib = OrionLib.getInstance();
         Connection connection = orionLib.createNewConnection("192.168.1.8", 3306, "orionlib", "root", "root");
 
-        try {
-            new QuerySet(connection, "create table if not exists usertest(name varchar(255), age int, primary key(name))", List.of());
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
         User user = new User("John", 30);
-
         try {
-            QueryGet<User> userQueryGet = new QueryGet<>(connection, "select * from usertest where name = ?", resultSet -> {
-                User user1 = null;
-                while (resultSet.next()) {
-                    user1 = new User(resultSet.getString("name"), resultSet.getInt("age"));
-                }
-
-                return user1;
-            }, List.of("John"));
-            System.out.println(userQueryGet.get().toString());
-
             ObjectUpdater<User> objectUpdater = new ObjectUpdater<>(connection, "usertest", user, user.getName());
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
